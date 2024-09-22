2e81625379bfa7bb925291ca8093d7a6e0371fc3
Sekiju <zzzcxk@gmail.com>
2024-09-22 17:34:14 +0500

    feat: ErrAuthorizationRequired

--- extension/manga/defs.go
+++ extension/manga/defs.go
@@ -1,11 +1,12 @@
 package manga
 
 const (
-	ErrUnimplemented   Error = "unimplemented method"
-	ErrForbidden       Error = "forbidden"
-	ErrMangaNotFound   Error = "manga not found"
-	ErrChapterNotFound Error = "chapter not found"
-	ErrPaidChapter     Error = "chapter is paid"
+	ErrUnimplemented         Error = "unimplemented method"
+	ErrForbidden             Error = "forbidden"
+	ErrMangaNotFound         Error = "manga not found"
+	ErrChapterNotFound       Error = "chapter not found"
+	ErrPaidChapter           Error = "chapter is paid"
+	ErrAuthorizationRequired Error = "authorization required"
 )
 
 func (e Error) Error() string {
--- extension/media/defs.go
+++ extension/media/defs.go
@@ -1,10 +1,11 @@
 package media
 
 const (
-	ErrUnimplemented   Error = "unimplemented method"
-	ErrForbidden       Error = "forbidden"
-	ErrMediaNotFound   Error = "media not found"
-	ErrEpisodeNotFound Error = "episode not found"
+	ErrUnimplemented         Error = "unimplemented method"
+	ErrForbidden             Error = "forbidden"
+	ErrMediaNotFound         Error = "media not found"
+	ErrEpisodeNotFound       Error = "episode not found"
+	ErrAuthorizationRequired Error = "authorization required"
 )
 
 func (e Error) Error() string {
